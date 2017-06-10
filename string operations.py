6adee32c9a7ea288f97489514d0e1365388d48e6
kshitij zutshi <kshitij.zutshi@gmail.com>
2017-06-10 13:57:36 +0530

    Add files via upload

---
+++ string operations.py
@@ -0,0 +1,13 @@
+##  STRING OPERATIONS- COMPARING STRINGS AND PRINTING RESPONSE
+
+
+name = "mcdonalds"
+# name1 = "king"
+len("kkkkkkk")
+
+print("hey there whats up?")
+if name is "mcdonalds" :
+    print("sorry not equal string, string length differs by :")
+
+elif name == "mcdonald" :
+    print ("strings are same..congrats!")
